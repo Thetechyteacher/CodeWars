3b934695ef863835e14551c42d2000ba8835ac10
Thetechyteacher <thetechyteacherr@gmail.com>
2023-03-28 10:57:33 +0100

    new KYU8

---
+++ KYU 8/helloWorld.js
@@ -0,0 +1,5 @@
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+function greet(){
+  return "hello world!"
+}
