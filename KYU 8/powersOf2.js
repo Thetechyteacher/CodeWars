4503f54883de2ec9f7ce0d00d633f0f02ef2fe4d
Thetechyteacher <thetechyteacherr@gmail.com>
2023-04-07 11:27:28 +0100

    new kata

---
+++ KYU 8/powersOf2.js
@@ -0,0 +1,10 @@
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+
+function powersOfTwo(n){
+    let listOfPowersOfTwo = []
+    for (let i = 0; i <= n; i++) {
+      listOfPowersOfTwo.push(2 ** i)
+    }
+    return listOfPowersOfTwo
+  }
