5e1919ca505fe455be6c43b24ecf73d59dc0af25
Thetechyteacher <thetechyteacherr@gmail.com>
2023-04-09 10:57:05 +0100

    new kyu7

---
+++ KYU 7/binary.js
@@ -0,0 +1,14 @@
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+//The binary number returned should be a string.
+
+//Examples:(Input1, Input2 --> Output (explanation)))
+
+
+
+
+function addBinary(a,b) {
+    const sum= a+b;
+      const binaryResult= sum.toString(2)
+      return binaryResult
+    }
